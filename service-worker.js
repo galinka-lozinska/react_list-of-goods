86bb061a2181f28baf04aa7936943da9da136d2e
Halynka Lozinska <galinkalozinska96@gmail.com>
2020-10-17 16:41:47 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6abdd38003b4e28b0c748d9b01572ff9.js"
+  "./precache-manifest.2c36f0fe2d61631ab6854122a84a8b02.js"
 );
 
 self.addEventListener('message', (event) => {
